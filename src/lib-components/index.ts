b72aa168af02a061d518c544abd2613824d4b0e2
Arslanoov <rasularslanoov@gmail.com>
2021-05-13 12:26:23 +0500

    build: Installed template

---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueAutocompleteInputSample } from './vue-autocomplete-input-sample.vue';
---
+++ vue-autocomplete-input.d.ts
@@ -0,0 +1,7 @@
+import { DefineComponent, Plugin } from 'vue';
+
+
+declare const VueAutocompleteInput: Exclude<Plugin['install'], undefined>;
+export default VueAutocompleteInput;
+
+export const VueAutocompleteInputSample: DefineComponent<{}, {}, any>;
